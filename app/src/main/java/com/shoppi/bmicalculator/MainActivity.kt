3a7eae2155982b128b0dc7f4a4084f80f834e5e5
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-05 22:51:43 +0900

    [2] confirm button

--- app/src/main/java/com/shoppi/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/shoppi/bmicalculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.shoppi.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.et_height)
+        val weightEditText: EditText = findViewById(R.id.et_weight)
+
+        val confirmButton = findViewById<Button>(R.id.bt_confirm)
+
+        confirmButton.setOnClickListener {
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+        }
     }
-}
+}1
